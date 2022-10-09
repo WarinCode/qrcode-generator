be372ff9eb118b9231121e0ab55f7a480c13644d
VarinCode <94356640+VarinCode@users.noreply.github.com>
2022-10-09 10:31:52 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 // import react
-import React,{ Component, ReactNode , Fragment} from 'react'
+import React,{ Component, ReactNode , Fragment} from 'react';
 
 // import component(default)
-import Navbar from './component/navbar'
-import Footer from './component/footer'
-import QR_Code from './component/qrcode'
+import Navbar from './component/navbar';
+import Footer from './component/footer';
+import QR_Code from './component/qrcode';
 
 // import lib
 import 'react-qr-code'
-import 'bootstrap/dist/css/bootstrap.min.css' 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'remixicon/fonts/remixicon.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'remixicon/fonts/remixicon.css';
 import 'sweetalert2/dist/sweetalert2.min.css';
-import 'sweetalert2/dist/sweetalert2.min.js'
+import 'sweetalert2/dist/sweetalert2.min.js';
 
 export default class App extends Component {
   render(): ReactNode {
-    return(
+    return (
       <Fragment>
         <Navbar/>
           <QR_Code/>
